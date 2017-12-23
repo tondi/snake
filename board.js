1866cd37307c33f036f575136cfadc7d8a9ad150
tondi <mtondos2@gmail.com>
2017-12-23 13:14:35 +0100

    Adds tile helper class

--- board.js
+++ board.js
@@ -1,44 +1,19 @@
+import Tile from './helpers/tile.js';
+
 export default class Board {
   constructor() {
-    // this.tiles = new Proxy([], {
-    //   set([x, y, newValue]) {
-    //     this.tiles = newValue;
-    //     console.log('new tiles:', this.tiles);
-    //     return true;
-    //   },
-    //   get() {
-    //     return this.tiles;
-    //   }
-    // });
-
-    // this.tiles = 1;
-
-    // Object.defineProperty(this, 'tiles', {
-    // });
-
-
+    this.tiles = [];
+    this.nativeElement = document.querySelector('.board');
   }
 
   createBoard() {
-    const board = [];
     for (let x = 0; x < 10; x++) {
-      board.push([]);
+      this.tiles.push([]);
       for (let y = 0; y < 10; y++) {
-        const html = `<div data-position-x="${x}" data-position-y="${y}" class="board__tile"></div>`;
-        const wrapper = document.createElement('div');
-        wrapper.innerHTML = html;
-        const tile = wrapper.firstElementChild;
-        board[x].push(tile);
+        const tile = new Tile(x, y);
+        this.tiles[x].push(tile);
+        this.nativeElement.appendChild(tile.nativeElement);
       }
     }
-
-    // this.board = board;
-    
-    const boardElement = document.querySelector('.board');
-    board.reduce((previousValue,   currentValue, index, array) => {
-      return previousValue.concat(currentValue);
-    }).map(el => {
-      boardElement.appendChild(el);
-    })
-  }  
+  }
 }
--- tile.js
+++
@@ -1,8 +0,0 @@
-export default class Tile {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.nativeElement = null;
-
-  }
-}
